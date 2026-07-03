49f8333a2af4ab4210fff4bde3b7695455af9e73
Teehxw <112659668+Teehxw@users.noreply.github.com>
2026-07-02 19:11:59 -0600

    Started the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,56 +1,32 @@
 import React from 'react';
 import {useState} from 'react';
-import { useEffect } from 'react';
+
 import './App.css';
-import {
-  HumanMessage, 
-  SystemMessage,
-  AIMessage, 
-  BaseMessage, 
-} from "@langchain/core/messagess";
 
 function App() {
   const [inputMess, setInputMess] = useState("")
 
-  const [messages, setMessages] = useState<BaseMessage>([]);
+  const [messages, setMessages] = useState([]);
 
   const handleClick = () => {
-    value = document.querySelector('.text-area').value;
-    alert('You typed: ' + value);
+    alert('You typed: ' + inputMess);
   }
 
   return (
     <div className= "App">
       <h1>Pitch Query</h1>
       <h2> Search through the World Cup History</h2>
-      <p> Welcome {name}!</p>
       <div className="chat-input">
-        <form className="chat-input">
-        <input>
+        <input
           type = "text"
           placeholder = "Type your query here..."
           value = {inputMess}
-          onChange = {(e) => setInputMess(e.target.value)}
-        </input>
-        <textarea className="text-area" placeholder="Type your query here..."></textarea>
-        <button className="button" onClick={handleClick}>Send</button>
-        </form>
+          onChange = {(e) => setInputMess(e.target.value)} />
+        <button className="button" onClick={handleClick}>Send</button> 
       </div>
 
     </div>
   );
 }
-
-// function Greeting() {
-//   const sayHi = (name, surname) => {
-//     alert('Hello ' + name + ' ' + surname + '!')
-//   }
-
-//   return (
-//     <div>
-//       <button onClick={() => sayHi('John','Doe')} > Say Hi</button>
-//     </div>
-//   )
-// }
 
 export default App;
